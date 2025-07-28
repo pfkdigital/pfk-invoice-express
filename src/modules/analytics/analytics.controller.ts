768d3b7a19d508716beaea3cda633bcc29f13110
NuhAli <nuh.ali@bt.com>
2025-07-28 21:04:08 +0100

    feat: add analytics endpoint root

--- src/modules/analytics/analytics.controller.ts
+++ src/modules/analytics/analytics.controller.ts
@@ -1,38 +1,67 @@
 import { NextFunction, Request, Response } from 'express';
 import * as analyticsService from './analytics.service';
 
-export const getInvoiceCount = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const count = await analyticsService.getInvoiceCount();
-        res.status(200).json({ count });
-    } catch (error) {
-        next(error);
-    }
-}
-
-export const getClientCount = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const count = await analyticsService.getClientCount();
-        res.status(200).json({ count });
-    } catch (error) {
-        next(error);
-    }
+export const getInvoiceCount = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const count = await analyticsService.getInvoiceCount();
+    res.status(200).json({ count });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getTotalAmount = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const total = await analyticsService.getTotalAmount();
-        res.status(200).json({ total });
-    } catch (error) {
-        next(error);
-    }
+export const getClientCount = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const count = await analyticsService.getClientCount();
+    res.status(200).json({ count });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getTotalAmountUnpaid = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const totalUnpaid = await analyticsService.getTotalAmountUnpaid();
-        res.status(200).json({ totalUnpaid });
-    } catch (error) {
-        next(error);
-    }
+export const getTotalAmount = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const total = await analyticsService.getTotalAmount();
+    res.status(200).json({ total });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getTotalAmountUnpaid = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const totalUnpaid = await analyticsService.getTotalAmountUnpaid();
+    res.status(200).json({ totalUnpaid });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAnalyticsData = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const data = await analyticsService.getAnalyticsData();
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
 };
--- src/modules/analytics/analytics.routes.ts
+++ src/modules/analytics/analytics.routes.ts
@@ -1,9 +1,13 @@
-import { Router } from "express";
+import { Router } from 'express';
 import * as analyticsController from './analytics.controller';
 
 export const analyticsRouter = Router();
 
+analyticsRouter.get('/', analyticsController.getAnalyticsData);
 analyticsRouter.get('/invoices/count', analyticsController.getInvoiceCount);
 analyticsRouter.get('/clients/count', analyticsController.getClientCount);
 analyticsRouter.get('/invoices/total', analyticsController.getTotalAmount);
-analyticsRouter.get('/invoices/total/unpaid', analyticsController.getTotalAmountUnpaid);
+analyticsRouter.get(
+  '/invoices/total/unpaid',
+  analyticsController.getTotalAmountUnpaid,
+);
